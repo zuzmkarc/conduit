ab350ef9246491cf9d7460251b7dff5c06dd6287
Tamas Jozsa <tjozsa@gmail.com>
2021-06-09 15:21:26 +0200

    adding cookie consent popup 	- It is required to ask permission from the user of Conduit to allow storing behavioural cookies on his computer. 	- poopup was implemented 	- cookie that stores the answer of the user: "vue-cookie-accept-decline-cookie-policy-panel", possible values ["null", "accept", "decline"]

--- src/vue/components/Footer.vue
+++ src/vue/components/Footer.vue
@@ -10,12 +10,43 @@
           >Thinkster</a
         >. Code &amp; design licensed under MIT.
       </span>
+      <vue-cookie-accept-decline
+        :ref="'cookie-policy'"
+        :elementId="'cookie-policy-panel'"
+        :debug="false"
+        :position="'bottom-left'"
+        :type="'bar'"
+        :disableDecline="false"
+        :transitionName="'slideFromBottom'"
+        :showPostponeButton="false"
+        :forceCookies="true"
+        @status="cookieStatus"
+        @clicked-accept="cookieClickedAccept"
+        @clicked-decline="cookieClickedDecline">
+
+        <!-- Optional -->
+        <div slot="message">
+            We use cookies to ensure you get the best experience on our website. <a href="https://cookiesandyou.com/" target="_blank">Learn More...</a>
+        </div>
+
+        <!-- Optional -->
+        <div slot="declineContent">
+          I decline!
+        </div>
+
+        <!-- Optional -->
+        <div slot="acceptContent">
+            I accept!
+        </div>
+    </vue-cookie-accept-decline>
     </div>
   </footer>
 </template>
 
 <script>
+import VueCookieAcceptDecline from 'vue-cookie-accept-decline'
 export default {
-  name: "Footer"
+  name: "Footer",
+  components: { VueCookieAcceptDecline }
 };
 </script>
