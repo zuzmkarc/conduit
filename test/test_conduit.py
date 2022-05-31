27db555a47daf02f31d06c08abbe856a05a77e7b
zuzmkarc <zuzmkarc@gmail.com>
2022-06-01 00:22:24 +0200

    refactor all

--- test/test_conduit.py
+++ test/test_conduit.py
@@ -40,7 +40,6 @@ class TestConduit(object):
         web_element.clear()
         return web_element
 
-    #
     # # TC-01 Registration with invalid email *** PASSED ***
     # def test_registration(self):
     #     sign_up_btn = WebDriverWait(self.browser, 2).until(
@@ -135,7 +134,7 @@ class TestConduit(object):
     #     article_to_comment = WebDriverWait(self.browser, 5).until(
     #         EC.presence_of_element_located((By.XPATH, '//a[@href="#/articles/zz" and @class="preview-link"]')))
     #     article_to_comment.click()
-    #     time.sleep(3)
+    #     time.sleep(4)
     #
     #     comment_field_xpath = '//textarea[@placeholder="Write a comment..."]'
     #     self.find_and_clear_element(comment_field_xpath).send_keys(test_user["comment"])
@@ -156,8 +155,9 @@ class TestConduit(object):
     #     trash_buttons = WebDriverWait(self.browser, 5).until(
     #         EC.presence_of_all_elements_located((By.XPATH, '//i[@class="ion-trash-a"]')))
     #     my_comment_trash_button = trash_buttons[-1]
+    #     time.sleep(3)
     #     my_comment_trash_button.click()
-
+    #
     # # TC-06 Delete element *** PASSED ***
     # def test_delete_element(self):
     #     self.login()
@@ -202,12 +202,12 @@ class TestConduit(object):
     #     articles_list_after_deletion = self.browser.find_elements_by_xpath('//div[@class="article-preview"]')
     #     articles_list_after_deletion_length = len(articles_list_after_deletion)
     #     assert articles_list_before_deletion_length - 1 == articles_list_after_deletion_length
-
+    #
     # # TC-07 Import data from csv file *** FAILED ***
     # def test_import_data_from_file(self):
     #     self.login()
     #     self.click_logged_in_user_name()
-    #     time.sleep(5)
+    #     time.sleep(3)
     #
     #     articles_before = WebDriverWait(self.browser, 5).until(
     #         EC.presence_of_all_elements_located((By.XPATH, '//div[@class="article-preview"]')))
@@ -225,7 +225,7 @@ class TestConduit(object):
     #     new_article_text_xpath = '//textarea[@placeholder="Write your article (in markdown)"]'
     #     new_article_tag_xpath = '//input[@placeholder="Enter tags"]'
     #
-    #     #with open("madardalok.csv", "r", encoding="UTF-8") as csvfile:
+    #     # with open("madardalok.csv", "r", encoding="UTF-8") as csvfile:
     #     with open("madardalok.csv", "r") as csvfile:
     #         csvreader = csv.reader(csvfile, delimiter=";")
     #         next(csvreader)
@@ -248,7 +248,39 @@ class TestConduit(object):
     #     time.sleep(3)
     #
     #     assert articles_after_length - 4 == articles_before_length
+    #
+    #     # DELETE NEWLY CREATED ARTICLES TO RESTORE ORIGINS:
+    #
+    #     article_one = WebDriverWait(self.browser, 5).until(
+    #         EC.presence_of_element_located((By.XPATH, '//a[@href="#/articles/a-feketerig-neke"]')))
+    #
+    #     article_one.click()
+    #     delete_article_btn = self.browser.find_element_by_xpath('//button[@class ="btn btn-outline-danger btn-sm"]')
+    #     delete_article_btn.click()
+    #     time.sleep(2)
+    #
+    #     article_two = WebDriverWait(self.browser, 5).until(
+    #         EC.presence_of_element_located((By.XPATH, '//a[@href="#/articles/az-szap-neke"]')))
+    #     article_two.click()
+    #     time.sleep(2)
+    #     delete_article_btn.click()
+    #     time.sleep(2)
+    #
+    #     article_three = WebDriverWait(self.browser, 5).until(
+    #         EC.presence_of_element_located((By.XPATH, '//a[@href="#/articles/a-vrsbegy-neke"]')))
+    #     article_three.click()
+    #     time.sleep(2)
+    #     delete_article_btn.click()
+    #     time.sleep(2)
+    #
+    #     article_four = WebDriverWait(self.browser, 5).until(
+    #         EC.presence_of_element_located((By.XPATH, '//a[@href="#/articles/a-zld-kll-neke"]')))
+    #     article_four.click()
+    #     time.sleep(2)
+    #     delete_article_btn.click()
+    #     time.sleep(2)
 
+    #
     # # TC-08 Update element (profile picture) *** FAILED *** confirm_btn-t újra meg kell adni a végén, nem ismeri fel
     # def test_update_profile_picture(self):
     #     self.login()
@@ -288,70 +320,69 @@ class TestConduit(object):
     #
     #     self.find_and_clear_element(profile_pic_field_xpath).send_keys(profile_pic_original)
     #
-    #     update_btn.click()
+    #     # update_btn.click()
+    #     # time.sleep(5)
     #     # confirm_btn.click()
     #
     #     confirm_btn = WebDriverWait(self.browser, 2).until(
     #         EC.presence_of_element_located((By.XPATH, '//button[@class="swal-button swal-button--confirm"]')))
     #     confirm_btn.click()
 
+    # TC-09 List elements *** FAILED *** time sleep nélkül rosszul adja vissza az elemek listáját
+    def test_list_elements(self):
+        self.login()
 
+        ipsum_tag = WebDriverWait(self.browser, 2).until(
+            EC.presence_of_element_located((By.XPATH, '//a[@href = "#/tag/ipsum"]')))
+        ipsum_tag.click()
+        time.sleep(2)
 
-    # # TC-09 List elements *** FAILED *** time sleep nélkül rosszul adja vissza az elemek listáját
-    # def test_list_elements(self):
-    #     self.login()
-    #
-    #     ipsum_tag = WebDriverWait(self.browser, 2).until(
-    #         EC.presence_of_element_located((By.XPATH, '//a[@href = "#/tag/ipsum"]')))
-    #     ipsum_tag.click()
-    #     time.sleep(2)
-    #
-    #     tagged_articles_list = WebDriverWait(self.browser, 3).until(
-    #         EC.presence_of_all_elements_located((By.XPATH, '//div[@class="article-preview"]')))
-    #
-    #     ipsum_tag_filter = WebDriverWait(self.browser, 3).until(
-    #         EC.presence_of_element_located((By.XPATH, '//a[@class="nav-link router-link-exact-active active"]')))
-    #
-    #     assert ipsum_tag_filter.is_displayed()
-    #     assert len(tagged_articles_list) == 3
-    #
-    # # TC-10 Pagination *** PASSED ***
-    # def test_pagination(self):
-    #     self.login()
-    #
-    #     pagination_buttons = WebDriverWait(self.browser, 2).until(
-    #         EC.presence_of_all_elements_located((By.XPATH, '//li/a[@class="page-link"]')))
-    #
-    #     page_one_btn = pagination_buttons[0]
-    #     page_two_btn = pagination_buttons[1]
-    #
-    #     active_button_color_value = "rgba(255, 255, 255, 1)"
-    #     inactive_button_color_value = "rgba(92, 184, 92, 1)"
-    #
-    #     page_one_btn.click()
-    #     time.sleep(2)
-    #
-    #     assert page_one_btn.value_of_css_property('color') == active_button_color_value
-    #     assert page_two_btn.value_of_css_property('color') == inactive_button_color_value
-    #
-    # # TC-11 Save data to file - collect user's articles' titles into txt file ***PASSED***
-    # def test_save_data_to_file(self):
-    #     self.login()
-    #
-    #     author = WebDriverWait(self.browser, 3).until(
-    #         EC.presence_of_all_elements_located((By.XPATH, '//a[@class="author" and  @href = "#/@testuser1/"]')))
-    #     author[0].click()
-    #     time.sleep(3)
-    #
-    #     author_articles_titles = WebDriverWait(self.browser, 3).until(
-    #         EC.presence_of_all_elements_located((By.XPATH, '//a[@class="preview-link"]//h1')))
-    #
-    #     with open("result.txt", "w", encoding='UTF-8') as result_content_empty:
-    #         for title in author_articles_titles:
-    #             result_content_empty.write(title.text)
-    #             result_content_empty.write('\n')
-    #
-    #     with open("result.txt", "r", encoding='UTF-8') as result_content_filled:
-    #         check_content = result_content_filled.readlines()
-    #
-    #     assert len(check_content) == len(author_articles_titles)
+        tagged_articles_list = WebDriverWait(self.browser, 3).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//div[@class="article-preview"]')))
+
+        ipsum_tag_filter = WebDriverWait(self.browser, 3).until(
+            EC.presence_of_element_located((By.XPATH, '//a[@class="nav-link router-link-exact-active active"]')))
+
+        assert ipsum_tag_filter.is_displayed()
+        assert len(tagged_articles_list) == 3
+
+    # TC-10 Pagination *** PASSED ***
+    def test_pagination(self):
+        self.login()
+
+        pagination_buttons = WebDriverWait(self.browser, 2).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//li/a[@class="page-link"]')))
+
+        page_one_btn = pagination_buttons[0]
+        page_two_btn = pagination_buttons[1]
+
+        active_button_color_value = "rgba(255, 255, 255, 1)"
+        inactive_button_color_value = "rgba(92, 184, 92, 1)"
+
+        page_one_btn.click()
+        time.sleep(2)
+
+        assert page_one_btn.value_of_css_property('color') == active_button_color_value
+        assert page_two_btn.value_of_css_property('color') == inactive_button_color_value
+
+    # TC-11 Save data to file - collect user's articles' titles into txt file ***PASSED***
+    def test_save_data_to_file(self):
+        self.login()
+
+        author = WebDriverWait(self.browser, 3).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//a[@class="author" and  @href = "#/@testuser1/"]')))
+        author[0].click()
+        time.sleep(3)
+
+        author_articles_titles = WebDriverWait(self.browser, 3).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//a[@class="preview-link"]//h1')))
+
+        with open("result.txt", "w", encoding='UTF-8') as result_content_empty:
+            for title in author_articles_titles:
+                result_content_empty.write(title.text)
+                result_content_empty.write('\n')
+
+        with open("result.txt", "r", encoding='UTF-8') as result_content_filled:
+            check_content = result_content_filled.readlines()
+
+        assert len(check_content) == len(author_articles_titles)
