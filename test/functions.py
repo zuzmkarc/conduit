503493fb2d6f5086eaca42eae9cea2db0ef5dfdb
zuzmkarc <zuzmkarc@gmail.com>
2022-06-08 19:26:19 +0200

    registration added test data refactored

--- test/functions.py
+++ test/functions.py
@@ -5,25 +5,54 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 
+def registration(browser, username, email, password):
+    sign_up_btn = WebDriverWait(browser, 6).until(
+        EC.presence_of_element_located((By.XPATH, '//a[@href="#/register"]')))
+    sign_up_btn.click()
+
+    username_field = WebDriverWait(browser, 6).until(
+        EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Username"]')))
+    username_field.clear()
+    username_field.send_keys(username)
+    email_field = WebDriverWait(browser, 6).until(
+        EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Email"]')))
+    email_field.clear()
+    email_field.send_keys(email)
+
+    password_field = WebDriverWait(browser, 6).until(
+        EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Password"]')))
+
+    password_field.clear()
+    password_field.send_keys(password)
+
+    login_btn = WebDriverWait(browser, 6).until(
+        EC.presence_of_element_located((By.XPATH, '//button[@class="btn btn-lg btn-primary pull-xs-right"]')))
+
+    login_btn.click()
+    confirm_btn = WebDriverWait(browser, 6).until (EC.presence_of_element_located((By.XPATH,'//button[@"swal-button swal-button--confirm"]')))
+    confirm_btn.click()
+    logout_btn = WebDriverWait(browser, 6).until (EC.presence_of_element_located((By.XPATH, '//a[@active-class="active"]')))
+    logout_btn.click()
+
 def login(browser, email, password):
-    sign_in_btn = browser.find_element_by_xpath('//a[@href="#/login"]')
+    sign_in_btn = WebDriverWait(browser, 6).until (EC.presence_of_element_located((By.XPATH, '//a[@href="#/login"]')))
     sign_in_btn.click()
     time.sleep(5)
 
-    email_field = browser.find_element_by_xpath('//input[@type="text"]')
+    email_field = WebDriverWait(browser, 6).until (EC.presence_of_element_located((By.XPATH, '//input[@type="text"]')))
     email_field.send_keys(email)
 
-    password_field = browser.find_element_by_xpath('//input[@type="password"]')
+    password_field = WebDriverWait(browser, 6).until (EC.presence_of_element_located((By.XPATH, '//input[@type="password"]')))
     password_field.send_keys(password)
 
-    login_btn = browser.find_element_by_xpath('//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+    login_btn = WebDriverWait(browser, 6).until (EC.presence_of_element_located((By.XPATH, '//button[@class="btn btn-lg btn-primary pull-xs-right"]')))
     login_btn.click()
     time.sleep(5)
 
 
 def click_logged_in_user_name(browser):
     logged_in_user_name = WebDriverWait(browser, 5).until(
-        EC.presence_of_element_located((By.XPATH, '//a[@href="#/@csokinyuszi/" and @class="nav-link"]')))
+        EC.presence_of_element_located((By.XPATH, '//a[@href="#/@Teszt/" and @class="nav-link"]')))
     logged_in_user_name.click()
     time.sleep(3)
 
--- test/test_data.py
+++ test/test_data.py
@@ -5,6 +5,9 @@ test_user = {
     "username_invalid": "testuser",
     "email_invalid": "test@gmail",
     "pwd_invalid": "Test2022",
+    "username_for_reg": "Teszt",
+    "pwd_for_reg": "Teszt123",
+    "email_for_reg": "tesztelo@gmail.com",
     "profile-pic": "https://news.cgtn.com/news/7a596a4e30677a4e7963444d7745444f786b444f31457a6333566d54/img/5fde4bcfe6bc4843b92ceb296a0b5201/5fde4bcfe6bc4843b92ceb296a0b5201.jpg"
 }
 
